01fbbf43f1f45d4c1fe54bc3c0848db56c0e8725
ControladoriaMaster <161758990+ControladoriaMaster@users.noreply.github.com>
2024-12-26 11:56:23 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from routes.home import home_blueprint
 from routes.estoque import estoque_blueprint
 from utils.database import get_connection, close_connection
+import requests
 
 def testar_conexao():
     """
@@ -25,7 +26,20 @@ app.register_blueprint(estoque_blueprint)
 def index():
     return render_template('index.html')
 
+def obter_ip_publico():
+    try:
+        response = requests.get('https://api64.ipify.org?format=json')
+        if response.status_code == 200:
+            ip = response.json().get('ip')
+            print(f"IP público do Render: {ip}")
+            return ip
+        else:
+            print(f"Erro ao obter IP público: {response.status_code}")
+    except Exception as e:
+        print(f"Erro ao conectar ao serviço de IP público: {e}")
+
 if __name__ == '__main__':
+    obter_ip_publico()
      # Testar conexão com o banco de dados
     testar_conexao()
     port = int(os.getenv("PORT", 5000))  # Render define a variável PORT
